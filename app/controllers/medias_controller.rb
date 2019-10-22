40e9eb234675030e576cd39069c3b95dce98c7e9
Ryan Rogers <ryansrogers85@gmail.com>
2019-10-22 16:45:25 -0400

    controller cleanup

--- app/controllers/medias_controller.rb
+++ app/controllers/medias_controller.rb
@@ -1,12 +1,8 @@
 class MediasController < ApplicationController
 
   def index
-    puts params[:user_public_user_id]
-
     @user = User.find_by(public_user_id: params[:user_public_user_id])
-    puts @user
     render json: {media: @user.medias}
-
   end
 
 
