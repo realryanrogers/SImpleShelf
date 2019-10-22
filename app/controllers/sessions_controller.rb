bb6e9324a4b5e53167a01966a047f0d4f09343d5
Ryan Rogers <ryansrogers85@gmail.com>
2019-10-22 10:54:24 -0400

    login and generate jwt

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,20 @@
+class SessionsController < ApplicationController
+
+  #skip_before_action :authenticate
+
+  def create
+    user = User.find_by(email: auth_params[:email])
+    if user.authenticate(auth_params[:password])
+      jwt = Auth.issue({user: user.public_user_id})
+      render json: {jwt: jwt}
+    else
+      render json: {status: 401}
+    end
+  end
+
+
+  private
+    def auth_params
+      params.require(:user).permit(:email, :password)
+    end
+end
