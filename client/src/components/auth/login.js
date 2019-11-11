bcef140939d2875cdd19daf4ab3cc43c9c35967a
Ryan Rogers <ryansrogers85@gmail.com>
2019-11-11 14:42:03 -0500

    link to reset password on login page

--- client/src/components/auth/login.js
+++ client/src/components/auth/login.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import axios from "axios";
+import { Link } from "react-router-dom"
 
 export default class Login extends Component {
   constructor(props) {
@@ -90,6 +91,7 @@ export default class Login extends Component {
           <button type="submit" className="btn btn-info">
             Login
           </button>
+          <Link to="/resetpassword" style={{ textDecoration: 'none', padding: '10px'}}>Forgot your password? </Link>
         </form>
       </div>
     );
--- db/schema.rb
+++ db/schema.rb
@@ -27,16 +27,6 @@ ActiveRecord::Schema.define(version: 2019_11_10_170515) do
     t.index ["user_id"], name: "index_media_on_user_id"
   end
 
-  create_table "profiles", force: :cascade do |t|
-    t.uuid "user_id", null: false
-    t.string "name"
-    t.integer "age"
-    t.string "city"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["user_id"], name: "index_profiles_on_user_id"
-  end
-
   create_table "users", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
     t.string "email"
     t.string "password_digest"
@@ -51,5 +41,4 @@ ActiveRecord::Schema.define(version: 2019_11_10_170515) do
   end
 
   add_foreign_key "media", "users"
-  add_foreign_key "profiles", "users"
 end
