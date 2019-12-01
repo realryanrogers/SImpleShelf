adcc1496714bfeae978292ff560172a6468ab721
Ryan <ryansrogers85@gmail.com>
2019-12-01 09:50:51 -0500

    adding public option to books

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_27_221552) do
+ActiveRecord::Schema.define(version: 2019_12_01_144706) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2019_11_27_221552) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.uuid "user_id", null: false
+    t.boolean "public", default: true
     t.index ["shelf_id"], name: "index_books_on_shelf_id"
     t.index ["user_id"], name: "index_books_on_user_id"
   end
